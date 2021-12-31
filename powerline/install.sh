0642a78b0fef6f33dceb9c379749a94025a770ed
Claire <krider2010@github.com>
2021-12-31 13:33:23 +0000

    Tweak installs and add powerline

---
+++ powerline/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Powerline fonts - useful for decent displays in iterm2 (and terminal)
+#
+# Various ones used - Menlo, Monaco, Roboto Mono
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+cp "Fira Coda Medium Nerd Font Complete.ttf" "$HOME/Library/Fonts/"
